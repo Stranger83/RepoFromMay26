40b65e2ed87d758920f5919d201c5793a3e90eb0
Stranger <jmanova83@gmail.com>
2017-05-31 16:09:40 +0300

    some new code

--- SMSTexting/Program.cs
+++ SMSTexting/Program.cs
@@ -10,7 +10,48 @@ namespace SMSTexting
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            string msg = "";
 
+            for (int i = 0; i < n; i++)
+            {
+                string letter = Console.ReadLine();
+                char button = letter[0];
+                switch (button)
+                {
+                    case '0':
+                        msg += " ";
+                        break;
+                    case '2':
+                       //letter.Length-3 = 'a';
+                        msg += "a";
+                        break;
+                    case '3':
+                        msg += "";
+                        break;
+                    case '4':
+                        msg += "";
+                        break;
+                    case '5':
+                        msg += "";
+                        break;
+                    case '6':
+                        msg += "";
+                        break;
+                    case '7':
+                        msg += "";
+                        break;
+                    case '8':
+                        msg += "";
+                        break;
+                    case '9':
+                        msg += "";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(msg);
         }
     }
 }
